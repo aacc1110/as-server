3f16a0a6665e74db6f17d0dbe60f9c45730ad55c
aacc1110 <tadrow@daum.net>
2019-06-21 19:48:27 +0900

    as-server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import './env';
 import app, { startSver } from './app';
 const { PORT } = process.env;
-startSver();
+
 app.listen(PORT, () => {
+  startSver();
   console.log('AS-service server is listening to port', PORT);
 });
