6192daf3ee25373da17e68c02bf86c1e72f3d730
aacc1110 <tadrow@daum.net>
2019-06-28 00:50:55 +0900

    as-server

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,7 @@ export const startSver = async () => {
   server.applyMiddleware({
     cors: {
       credentials: true,
-      origin: 'http://localhost:3005',
+      origin: 'http://localhost:3000',
       allowHeaders: ['Content-Type', 'Authorization']
     },
     app
