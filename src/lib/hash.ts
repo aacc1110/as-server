1504c43f2f20d5f8c1f98819c238a5c0ce1fe9ca
aacc100400 <tadrow@naver.com>
2020-03-08 17:50:20 +0900

    as-server

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,9 +1,6 @@
 import crypto from 'crypto';
 
 const { HASH_KEY } = process.env;
-if (!HASH_KEY) {
-  throw new Error('Please set HASH_KEY Environment variable');
-}
 
 export default function hash(text: string) {
   if (!HASH_KEY) {
